6bd6029d234890510096bff847dc54f6a0bd8185
Igor1997Klimenko <Igorek.klimuch.97@gmail.com>
2023-04-03 16:16:45 +0300

    build

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/marvel_project'>
+    <BrowserRouter basename='/marvel_project.com'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
